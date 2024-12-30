50bb20751ecee0a3377f3430318bb66e346c560f
Kundan Kumar <kundankr103060@gmail.com>
2024-12-30 19:23:37 +0530

    Create MaxElement.java

---
+++ MaxElement.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class MaxElement {
+
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int numOfElements = Integer.valueOf(br.readLine());
+
+		int arr[] = Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+
+		int max = arr[0];
+		for (int i = 1; i < numOfElements - 1; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		System.out.print(max);
+
+	}
+}
